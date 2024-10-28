898304388dc9c9334baf4f78d2d381a2614ebf3d
carloscdf <valdelicebatista60@gmail.com>
2024-10-27 22:56:20 -0300

    Adição de links e footer adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { TopBannerComponent } from './components/top-banner/top-banner.component';
 import { AlbumCoversComponent } from './components/album-covers/album-covers.component';
 import { SelectedAlbumComponent } from './components/selected-album/selected-album.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopBannerComponent, AlbumCoversComponent, SelectedAlbumComponent],
+  imports: [RouterOutlet, HeaderComponent, TopBannerComponent, AlbumCoversComponent, SelectedAlbumComponent, FooterComponent],
   templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
